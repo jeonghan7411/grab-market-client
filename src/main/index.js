5f8cb22d32d13ff5695761a17a4a522548cbf2d7
Jeong han Bang <aufkrh@Naver.com>
2022-09-15 23:00:02 +0900

    antd 기본적용

--- src/main/index.js
+++ src/main/index.js
@@ -7,7 +7,7 @@ function MainPage() {
   React.useEffect(function () {
     axios
       .get(
-        "https://f5c3b144-5ec3-4a12-a6e3-b40503cf8b37.mock.pstmn.io/products"
+        "https://f5c3b144-5ec3-4a12-a6e3-b40503cf8b37.mock.pstmn.io/products/ "
       )
       .then(function (result) {
         const products = result.data.products;
@@ -16,52 +16,40 @@ function MainPage() {
       .catch(function (error) {
         console.error("에러 발생 : ", error);
       });
-  }, []);
+  }, []); //[] 값을 비워두게 되면 한번, 처음 렌더링에만 호출됨
 
   return (
     <div>
       {" "}
-      <div class="header">
-        <div class="header-area">
-          <img src="/images/icons/logo.png" alt="" />
-        </div>
+      <div class="banner">
+        <img src="/images/banners/banner1.png" alt="" />
       </div>
-      <div class="body">
-        <div class="banner">
-          <img src="/images/banners/banner1.png" alt="" />
-        </div>
-        <h1>판매되는 상품들</h1>
-        <div class="product-list">
-          {products.map(function (product, index) {
-            return (
-              <div className="product-card">
-                <Link className="product-link" to={`/products/${index}`}>
-                  <div>
+      <h1>판매되는 상품들</h1>
+      <div class="product-list">
+        {products.map(function (product, index) {
+          return (
+            <div className="product-card">
+              <Link className="product-link" to={`/products/${product.id}`}>
+                <div>
+                  <img src={product.imageUrl} className="product-img" alt="" />
+                </div>
+                <div className="products-contents">
+                  <span className="product-name">{product.name}</span>
+                  <span className="product-price">{product.price}원</span>
+                  <div className="product-seller">
                     <img
-                      src={product.imageUrl}
-                      className="product-img"
+                      className="product-avatar"
+                      src="images/icons/avatar.png"
                       alt=""
                     />
+                    <span>{product.seller}</span>
                   </div>
-                  <div className="products-contents">
-                    <span className="product-name">{product.name}</span>
-                    <span className="product-price">{product.price}원</span>
-                    <div className="product-seller">
-                      <img
-                        className="product-avatar"
-                        src="images/icons/avatar.png"
-                        alt=""
-                      />
-                      <span>{product.seller}</span>
-                    </div>
-                  </div>
-                </Link>
-              </div>
-            );
-          })}
-        </div>
+                </div>
+              </Link>
+            </div>
+          );
+        })}
       </div>
-      <div class="footer"></div>
     </div>
   );
 }
